fa946adc8c73071f262d2378e445ab9026084f14
venkatesh2508 <70590619+venkatesh2508@users.noreply.github.com>
2022-03-15 18:22:43 +0530

    Add files via upload

---
+++ BLOODDONATION.java
@@ -0,0 +1,17 @@
+
+public class BLOODDONATION {
+   public static void main(String[] args)
+   {
+	   int age = 18;
+	   int weight = 60;
+	   
+	if (age>=18 && weight>=60)
+	{
+		System.out.println("eligible");
+	}
+	else
+	{
+		System.out.println("not eligible");
+	}
+   }
+}
